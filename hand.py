edcaae4e25534da845e626809ad717abebde876a
James Y <abcde201220122012@outlook.com>
2016-08-06 14:07:42 -0700

    updates
    make hand.cards private by turning it into hand.__cards

--- hand.py
+++ hand.py
@@ -6,24 +6,31 @@ from card import card
 class hand:
     def __init__(self, cards = []):
         'Initialize cards'
-        self.cards = sorted(cards, key = card.sortKey) # Clb-Dmnd-Spd-Hrt
+        self.__cards = sorted(cards, key = card.sortKey) # Clb-Dmnd-Spd-Hrt
     def playCardByIndex(self, index):
-        temp = self.cards[index]
-        del self.cards[index]
+        temp = self.__cards[index]
+        del self.__cards[index]
         return temp
     def playCardByCard(self, card):
-        for i in range(len(self.cards)):
-            if self.cards[i] == card: return self.playCardByInd(i)
+        for i in range(len(self.__cards)):
+            if self.__cards[i] == card: return self.playCardByInd(i)
     def giveThreeCards(self, indeces, target):
         'Give three cards out to target'
         if len(indeces) - 3: raise ValueError # if length != 3, error
-        given = [self.cards[i] for i in indeces]
-        for i in indeces: del self.cards[i]
+        given = [self.__cards[i] for i in indeces]
+        for i in indeces: del self.__cards[i]
         target.obtainCards(given)
     def obtainCards(self, cards):
         'Obtain cards'
-        self.cards += cards
-        self.cards = sorted(self.cards, key = card.sortKey)
-
+        self.__cards += cards
+        self.__cards = sorted(self.__cards, key = card.sortKey)
+    #############################################################################################
+    ################################ METHODS FOR DEBUGGING ONLY #################################
+    #############################################################################################
+    @staticmethod
+    def __debug_init(cardsSeries = []):
+        'FOR DEBUG ONLY. Takes in 13 series numbers of cards'
+        if type(cardsSeries) != list or len(cardsSeries) > 51: raise ValueError
+        return hand([card().setSeries(n) for n in cardsSeries])
 
 pass
