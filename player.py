0823ce3402353d82e08597b2b9ff838a49fc0aec
James Y <abcde201220122012@outlook.com>
2016-08-11 04:02:09 -0700

    updates

--- player.py
+++ player.py
@@ -13,6 +13,6 @@ class player:
         self.handOfCards = handOfCards
         self.actionModule = human if ifHumanPlayer else ai
     def handOutCards(self, overAllScore, targetPlayerNum):
-        return self.actionModule.handOutCards(self.handOfCards)
+        return self.actionModule.handOutCards(self.handOfCards, overAllScore, targetPlayerNum)
     def playCards(self, othersCards, overAllScore):
-        return self.actionModule.playCards(self.handOfCards)
+        return self.actionModule.playCards(self.handOfCards, othersCards, overAllScore)
