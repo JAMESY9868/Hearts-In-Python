d83edca44f07253eb47e31c8e10a046af5a9a3f4
James Y <abcde201220122012@outlook.com>
2016-08-11 03:50:50 -0700

    updates

--- ai.py
+++ ai.py
@@ -3,33 +3,48 @@
 
 from hand import hand
 
-#####################################################################################################################################
-#################################################### START OF handOutScores DATA ####################################################
-#####################################################################################################################################
+##########################################################################################################################
+################################################ START OF cardValues DATA ################################################
+##########################################################################################################################
 # the data down are experimental
-handOutScores = {} # Spade Queen
-for i in range(7): handOutScores[(0, i)] = -50 + 3 * i # spade 2 to 8 -- because there is a spade queen, scores in Spades are extreme
-for i in range(7, 10): handOutScores[(0, i)] = -30 + 3 * i # spade 9 to J
-handOutScores[(0, 10)] = 100 # spade queen
-for i in range(11, 13): handOutScores[(0, i)] = 50 + 3 * i # spade K and A -- same reason it is extreme
-for i in range(7): handOutScores[(1, i)] = -3 + i // 2 # heart 2 to 8
-for i in range(7, 13): handOutScores[(1, i)] = i // 2 # heart 9 to A
+cardValues = {} # Spade Queen
+for i in range(7): cardValues[(0, i)] = -50 + 3 * i # spade 2 to 8 -- because of spade queen, scores in Spades are extreme
+for i in range(7, 10): cardValues[(0, i)] = -30 + 3 * i # spade 9 to J
+cardValues[(0, 10)] = 100 # spade queen
+for i in range(11, 13): cardValues[(0, i)] = 50 + 3 * i # spade K and A -- for the same reason it is extreme
+for i in range(7): cardValues[(1, i)] = -3 + i // 2 # heart 2 to 8
+for i in range(7, 13): cardValues[(1, i)] = i // 2 # heart 9 to A
 for i in range(7): 
-    for j in range(2, 4): handOutScores[(j, i)] = -12 + i - j # diamond and club, clubs are 1 score higher
+    for j in range(2, 4): cardValues[(j, i)] = -12 + i - j # diamond and club, clubs are 1 score higher
 for i in range(7, 13): 
-    for j in range(2, 4): handOutScores[(j, i)] = -5 + i + 2 * j # diamond and club, clubs are 2 score lower
-handOutScores[(2, 0)] = 5 # club 2 for starting off the game
-#####################################################################################################################################
-##################################################### END OF handOutScores DATA #####################################################
-#####################################################################################################################################
+    for j in range(2, 4): cardValues[(j, i)] = -5 + i + 2 * j # diamond and club, clubs are 2 score lower
+cardValues[(2, 0)] = 5 # club 2 for starting off the game
+###########################################################################################################################
+################################################# END OF cardValues DATA ##################################################
+###########################################################################################################################
 
-def handOutCards(handOfCards, overAllScore = [0, 0, 0, 0]):
+def handOutCards(handOfCards, overAllScore, targetPlayerNum):
     'The ai algorithm for handing out cards, returns three "card" objects'
     # evaluate how many high heart cards there is and if there is SQ
     # if ther is not enough high cards, try to get rid of high cards by score
     # otherwise try to get rid of lowest cards
     
 
-def playCards(handOfCards, othersCards = [], overAllScore = [0, 0, 0, 0]):
+def playCards(handOfCards, othersCards, overAllScore):
     'the ai algorithm for playing out cards'
+    ##########################################################################################
+    # try to play the lowest high cards
+    # get rid of high cards as fast as possible, maybe taking some risks
+    # get rid of hearts (and high spades) first
+    # if other people play harmless cards, play highest
+    # if inevitable gain of score, play highest
+    # account for probability of upcoming people playing low cards, and decide
+    #+ whether play low or high cards (the risk-taking part)
+    ##########################################################################################
+    # try not to finish a suite
+    # determine if possible (whether hand is high enouth, & whether someone else got scores)
+    # save 1/2 high cards for each suite for score-gaining
+    # for scoreless turns play lowest card
+    # if negative-value cards' amount reduces to 5, start to play highest cards
+    ##########################################################################################
 
