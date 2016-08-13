245e6b6caa75e6da31e0614121ce04aed80bbc59
ookami-sama <1142991276@qq.com>
2016-08-13 00:00:53 -0700

    updates

--- game.py
+++ game.py
@@ -3,10 +3,38 @@
 
 from deck import deck # a deck of cards
 from hand import hand # unsure if necessary, as not being explicitly used
+from player import player
 
 def main():
-    result = input('Please type in your name: ')
-    
+    players=[player(cards) for cards in deck().fourHands()]
+    players[0].setMode(True)
+    for i in range(0,4):
+        players[i].setAllPlayers(players)
+        players[i].setPlayerNum(i)
+    overAllScore = [0, 0, 0, 0]
+    playedRounds = 0
+    while True not in [i > 99 for i in overAllScore]:
+        playedRounds += 1
+        handOutTurn(players, overAllScore, playedRounds)
+        playTurn(players, overAllScore, playedRounds)
+def handOutTurn(players, overAllScore, playedRounds):
+    lst=[]
+    for i in range(0,4):
+        lst+=players[i].handOutCards(overAllScore, [1, 3, 2, 0][(playedRounds - 1) % 4])
+    for i in range(0,4):
+        if lst[0]: # get rid of the card
+            for crd in lst[i]: players[i].handOfCards.playCardByCard(crd)
+        for crd in lst[i]: players[(i - [1, 3, 2, 0][(playedRounds - 1) % 4]) % 4].handOfCards.obtainCards(crd)
+def playTurn(players, overAllScore, playedRounds):
+    for i in range(13):
+        othersCards = []
+        for j in range(4):
+            othersCards += players[j].playCards(othersCards, overAllScore)
+        high = othersCards[0]
+        for k in range(3):
+            high = othersCards[k] if othersCards[k].compare(high) else high
+        scores = sum([crd.score() for crd in othersCards])
+            
 
 
 
--- player.py
+++ player.py
@@ -11,9 +11,17 @@ class player:
     def __init__(self, handOfCards, ifHumanPlayer = False):
         'handOfCards: a "hand" of cards; ifHumanPlayer: whether this player is human'
         self.handOfCards = handOfCards
-        self.actionModule = human if ifHumanPlayer else ai
         self.score = 0
+        self.allPlayers=[]
+        self.playerNum=-1
+        self.setMode(ifHumanPlayer)
+    def setMode(ifHumanPlayer = False):        
+        self.actionModule = human if ifHumanPlayer else ai
+    def setAllPlayers(self, allPlayers):
+        self.allPlayers=allPlayers
+    def setPlayerNum(self, playerNum):
+        self.playerNum=playerNum
     def handOutCards(self, overAllScore, targetPlayerNum):
-        handedCards = self.actionModule.handOutCards(self.handOfCards, overAllScore, targetPlayerNum)
+        return self.actionModule.handOutCards(self.handOfCards, overAllScore, targetPlayerNum)
     def playCards(self, othersCards, overAllScore):
-        playedCard = self.actionModule.playCards(self.handOfCards, othersCards, overAllScore)
+        return self.actionModule.playCards(self.handOfCards, othersCards, overAllScore)
