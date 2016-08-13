26697f1231d3dd4d91d739f21e1f7291669b9b4e
James Y <abcde201220122012@outlook.com>
2016-08-13 00:03:36 -0700

    updates

--- dataDisplay.py
+++ dataDisplay.py
@@ -3,12 +3,6 @@
 
 from shutil import get_terminal_size as terminalSize # the size of the terminal window
 
-def display(players):
-    termSize = terminalSize()
-    if termSize[0] < 60 or termSize[1] < 10:
-        print('Terminal size too small. Please enlarge your terminal window to proceed.' + \
-            '\nCurrent terminal Size: columns={}, lines={}' % tuple(termSize))
-        raise ValueError
 def _debugTerminalSizeDisplay():
     print('Columns: %d, lines: %d' % tuple(terminalSize()))
 
@@ -21,4 +15,4 @@ def screenDataDump(dataNum, dataStr):
     #   1, dumping to the middle section
     #   2, dumping to the lower section
     #################################################
-    
+    print(dataStr)
